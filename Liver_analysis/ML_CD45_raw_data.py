1c25e848a243ca7f1f745ca4a8dea951e9991548
Thomas <thomas.koc92@gmail.com>
2024-07-30 00:42:04 +0100

    NN trained for raw data

--- Liver_analysis/ML_CD45_raw_data.py
+++ Liver_analysis/ML_CD45_raw_data.py
@@ -3,8 +3,25 @@ from sklearn.model_selection import train_test_split
 from keras.models import Sequential
 from keras.layers import Dense
 import pandas as pd
+import scipy.sparse as sp
 from keras.layers import Dropout
 from scipy.io import mmread
+from keras.utils import Sequence
+
+
+class DataGenerator(Sequence):
+    def __init__(self, X, y, batch_size):
+        self.X = X
+        self.y = y
+        self.batch_size = batch_size
+
+    def __len__(self):
+        return int(np.ceil(len(self.X) / float(self.batch_size)))
+
+    def __getitem__(self, idx):
+        batch_X = np.asarray(self.X[idx * self.batch_size:(idx + 1) * self.batch_size])
+        batch_y = np.asarray(self.y[idx * self.batch_size:(idx + 1) * self.batch_size])
+        return batch_X, batch_y
 
 
 # Load the matrix.mtx file
@@ -41,17 +58,40 @@ annot_data_labels = [s.strip('"') for s in annot_data_labels][1:]
 
 # Load the sparse matrix into a DataFrame
 sparse_df = pd.DataFrame.sparse.from_spmatrix(filtered_matrix_csr)
-sparse_df = pd.DataFrame.sparse.from_spmatrix(sparse_df.sparse.to_coo().transpose(), index=filtered_barcodes_data, columns=df_features['name'])
+df = sparse_df.sparse.to_coo().transpose()
+df = pd.DataFrame.sparse.from_spmatrix(df, index=filtered_barcodes_data, columns=df_features['name'])
 
 # Add a new column 'healthy' to df
-sparse_df['type'] = annot_data_labels
-sparse_df['healthy'] = sparse_df['type'].str.startswith("SD").astype(int)
+df['type'] = annot_data_labels
+df['healthy'] = df['type'].str.startswith("SD").astype(int)
 # Remove the 'type' column from sparse_df
-sparse_df = sparse_df.drop('type', axis=1)
+df = df.drop('type', axis=1)
 #print(sparse_df.sample(n=20))
+""" 
+# Convert df to a CSR matrix
+csr_matrix = pd.DataFrame.sparse.from_spmatrix(df).sparse.to_csr()
 
-dataset = sparse_df.drop('healthy', axis=1)
-labels = sparse_df['healthy']
+# Create a new sparse DataFrame from the CSR matrix
+df = pd.DataFrame.sparse.from_spmatrix(csr_matrix, index=filtered_barcodes_data, columns=df_features['name'])
+
+# Make the DataFrame sparse
+csr_matrix = sp.coo_matrix(df.values)
+sparse_df = pd.DataFrame.sparse.from_spmatrix(csr_matrix, index=filtered_barcodes_data, columns=df_features['name'])
+
+# Store sparse_df to local storage
+csr_matrix = df.sparse.to_coo().tocsr()
+#sp.save_npz('/home/thomas/Documents/Imperial/Thesis/Project_repo/data/sparse_df.npz', csr_matrix)
+print("Dataframe saved to local storage")
+
+
+# Load sparse_df from local storage
+sparse_df = pd.read_csv('/home/thomas/Documents/Imperial/Thesis/Project_repo/data/sparse_df.csv')
+ """
+labels = df['healthy']
+dataset = df.drop('healthy', axis=1)
+
+del sparse_df
+del df
 
 print("Before splitting the dataset")
 
@@ -71,7 +111,16 @@ model.add(Dense(1, activation='sigmoid'))
 # Compile the model
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+# Define the generator
+train_generator = DataGenerator(X_train, y_train, batch_size=64)
+
 print("Before training the model")
+del dataset
+del labels
 
 # Train the model
-model.fit(X_train, y_train, epochs=10, batch_size=32, validation_data=(X_test, y_test))
+model.fit(train_generator, epochs=5, validation_data=(X_test, y_test))
+
+# Save the model to local storage
+model.save('/home/thomas/Documents/Imperial/Thesis/Project_repo/data/model_raw.h5')
+print("Model saved to local storage")
